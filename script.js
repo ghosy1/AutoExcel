428f92119bd4d83cdb46050a7cde9db0a411713f
ghosy1 <139178039+ghosy1@users.noreply.github.com>
2023-09-27 22:55:01 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,151 @@
 const excel_file = document.getElementById('excel_file');
 const arrange = document.getElementById('arrange')
 const newList = document.getElementById('newList')
+var z = 0
+
+function rearrange() {
+    
+        var rows = document.querySelectorAll('tr')
+        
+        
+         for (var n=0; n<rows.length; n++)
+         {  
+            var cell = rows[n].querySelectorAll('td')
+            if (cell[3])
+            {if (cell[3].textContent == "M" || cell[3].textContent == "F"){
+            var room = cell[1].textContent
+            var name = cell[2].textContent
+            var gender = cell[3].textContent
+            if (gender == "F"){gender = "Ž"}
+            var country = cell[4].textContent
+            country = country.toLowerCase()
+            country = country.charAt(0).toUpperCase() + country.slice(1)
+            var age = cell[5].textContent   
+            age = age.substring(0, age.indexOf(","));
+            var passport = cell[6].textContent
+            var firstTwo = passport.slice(0,2)
+            if (firstTwo == "2 " ) {passport = "P:" + passport.slice(2)}
+            if (firstTwo == "27" ) {passport = "LK:" + passport.slice(2)}
+            if (firstTwo == "6 " ) {passport = "DP:" + passport.slice(2)}
+            if (firstTwo == "32" ) {passport = "VD:" + passport.slice(2)}
+            
+    
+            
+            var newListRow = document.createElement("tr")
+            newListRow.classList.add("list")
+            newListRow.setAttribute('id',room);
+            newList.appendChild(newListRow)
+    
+            var newListCell = document.createElement("td")
+            newListCell.style.padding="3px"
+            newListRow.appendChild(newListCell)
+            newListCell.textContent= name
+    
+            var newListCell = document.createElement("td")
+            newListCell.style.padding="3px"
+            newListRow.appendChild(newListCell)
+            newListCell.textContent= country
+    
+            var newListCell = document.createElement("td")
+            newListCell.style.padding="3px"
+            newListRow.appendChild(newListCell)
+            newListCell.textContent= passport
+    
+            var newListCell = document.createElement("td")
+            newListCell.style.padding="3px"
+            newListRow.appendChild(newListCell)
+            newListCell.textContent= age
+    
+            
+            var newListCell = document.createElement("td")
+            newListCell.style.padding="3px"
+            newListRow.appendChild(newListCell)
+            newListCell.textContent= gender
+            if (newListCell.textContent=="Ž") {newListCell.style.color="red"}
+           
+            }
+        else if (cell[4].textContent == "M" || cell[4].textContent == "F"){
+            var room = cell[1].textContent
+            var name = cell[2].textContent
+            var gender = cell[4].textContent
+            if (gender == "F"){gender = "Ž"}
+            var country = "Bosna i Hercegovina"
+            var age = cell[5].textContent   
+            age = age.substring(0, age.indexOf(","));
+            var passport = cell[6].textContent
+            passport = passport.substring(0, passport.indexOf(","))
+            
+            
+    
+            
+            var newListRow = document.createElement("tr")
+            newListRow.classList.add("list")
+            newListRow.setAttribute('id',room);
+            newList.appendChild(newListRow)
+    
+            var newListCell = document.createElement("td")
+            newListCell.style.padding="3px"
+            newListRow.appendChild(newListCell)
+            newListCell.textContent= name
+    
+            var newListCell = document.createElement("td")
+            newListCell.style.padding="3px"
+            newListRow.appendChild(newListCell)
+            newListCell.textContent= country
+    
+            var newListCell = document.createElement("td")
+            newListCell.style.padding="3px"
+            newListRow.appendChild(newListCell)
+            newListCell.textContent= passport
+    
+            var newListCell = document.createElement("td")
+            newListCell.style.padding="3px"
+            newListRow.appendChild(newListCell)
+            newListCell.textContent= age
+    
+            
+            var newListCell = document.createElement("td")
+            newListCell.style.padding="3px"
+            newListRow.appendChild(newListCell)
+            newListCell.textContent= gender
+            if (newListCell.textContent=="Ž") {newListCell.style.color="red"}
+        }
+        }
+            
+            
+            
+    
+            
+          }
+        var list = document.querySelectorAll(".list")
+    
+            var arr = []
+            i=0
+            list.forEach(listItem=>{
+                arr.push(listItem.outerHTML)
+                }
+            )
+    
+            arr.sort()
+            list.forEach(element => {
+                element.innerHTML=arr[i]
+                i++
+                
+            });
+         rows.forEach(row=>{
+            if (row.id!="list") {row.outerHTML=""}
+         })
+    
+
+}
+
+function ExportToExcel(type, fn, dl) {
+    var elt = document.getElementById('tbl_exporttable_to_xls');
+    var wb = XLSX.utils.table_to_book(elt, { sheet: "sheet1" });
+    return dl ?
+        XLSX.write(wb, { bookType: type, bookSST: true, type: 'base64' }) :
+        XLSX.writeFile(wb, fn || ('MySheetName.' + (type || 'xlsx')));
+}
 
 excel_file.addEventListener('change', (event) => {
 
@@ -60,7 +205,7 @@ excel_file.addEventListener('change', (event) => {
 
             table_output += '</table>';
 
-            document.getElementById('excel_data').innerHTML = table_output;
+            document.getElementById('excel_data').innerHTML += table_output;
         }
 
         excel_file.value = '';
@@ -69,90 +214,9 @@ excel_file.addEventListener('change', (event) => {
 
 });
 
-function ExportToExcel(type, fn, dl) {
-    var elt = document.getElementById('tbl_exporttable_to_xls');
-    var wb = XLSX.utils.table_to_book(elt, { sheet: "sheet1" });
-    return dl ?
-        XLSX.write(wb, { bookType: type, bookSST: true, type: 'base64' }) :
-        XLSX.writeFile(wb, fn || ('MySheetName.' + (type || 'xlsx')));
-}
 
 arrange.addEventListener("click", ()=>{
-    var rows = document.querySelectorAll('tr')
-    
-    
-     for (var n=2; n<rows.length; n++)
-     {  
-        var cell = rows[n].querySelectorAll('td')
-        var room = cell[1].textContent
-        var name = cell[2].textContent
-        var gender = cell[3].textContent
-        if (gender == "F"){gender = "Ž"}
-        var country = cell[4].textContent
-        country = country.toLowerCase()
-        country = country.charAt(0).toUpperCase() + country.slice(1)
-        var age = cell[5].textContent
-        age = age.substring(0, age.indexOf(","));
-        var passport = cell[6].textContent
-        var firstTwo = passport.slice(0,2)
-        if (firstTwo == "2 " ) {passport = "P:" + passport.slice(2)}
-        if (firstTwo == "27" ) {passport = "LK:" + passport.slice(2)}
-        if (firstTwo == "6 " ) {passport = "DP:" + passport.slice(2)}
-        if (firstTwo == "32" ) {passport = "VD:" + passport.slice(2)}
-        
-
-        if (gender != "Spol"){
-        var newListRow = document.createElement("tr")
-        newListRow.classList.add("list")
-        newListRow.setAttribute('id',room);
-        newList.appendChild(newListRow)
-
-        var newListCell = document.createElement("td")
-        newListCell.style.padding="3px"
-        newListRow.appendChild(newListCell)
-        newListCell.textContent= name
-
-        var newListCell = document.createElement("td")
-        newListCell.style.padding="3px"
-        newListRow.appendChild(newListCell)
-        newListCell.textContent= country
-
-        var newListCell = document.createElement("td")
-        newListCell.style.padding="3px"
-        newListRow.appendChild(newListCell)
-        newListCell.textContent= passport
-
-        var newListCell = document.createElement("td")
-        newListCell.style.padding="3px"
-        newListRow.appendChild(newListCell)
-        newListCell.textContent= age
-
-        
-        var newListCell = document.createElement("td")
-        newListCell.style.padding="3px"
-        newListRow.appendChild(newListCell)
-        newListCell.textContent= gender
-
-       }
-        
-        
-
-        
-      }
-    var list = document.querySelectorAll(".list")
-
-        var arr = []
-        i=0
-        list.forEach(listItem=>{
-            arr.push(listItem.outerHTML)
-            }
-        )
-
-        arr.sort()
-        list.forEach(element => {
-            element.innerHTML=arr[i]
-            i++
-            
-        });
-        console.log(arr);
+    rearrange()
 })
+
+
